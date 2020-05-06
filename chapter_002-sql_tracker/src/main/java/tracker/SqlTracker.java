2d88e818008440a5d4c5b247c24336bfcc7a6630
DenisViskov <DenisViskov16@gmail.com>
2020-05-06 19:54:50 +0300

    added SQLTracker and app.properties

---
+++ chapter_002-sql_tracker/src/main/java/tracker/SqlTracker.java
@@ -0,0 +1,67 @@
+package tracker;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * @author Денис Висков
+ * @version 1.0
+ * @since 06.05.2020
+ */
+public class SqlTracker implements Store {
+    private Connection cn;
+
+    @Override
+    public void init() {
+        try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("app.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            cn = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public Item add(Item item) {
+        return null;
+    }
+
+    @Override
+    public boolean replace(String id, Item item) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return false;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        return null;
+    }
+
+    @Override
+    public Item findById(String id) {
+        return null;
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
